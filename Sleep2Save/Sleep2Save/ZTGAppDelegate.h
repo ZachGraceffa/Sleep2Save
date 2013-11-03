c917bb3257837f7ede827770ab92eeb41840b582
Zach Graceffa <zachgraceffa@gmail.com>
2013-11-03 15:12:43 -0600

    Empty XCode Project Created

---
+++ Sleep2Save/Sleep2Save/ZTGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZTGAppDelegate.h
+//  Sleep2Save
+//
+//  Created by Zachary Graceffa on 11/3/13.
+//  Copyright (c) 2013 Zachary Graceffa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZTGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sleep2Save/Sleep2Save/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sleep2Save
+//
+//  Created by Zachary Graceffa on 11/3/13.
+//  Copyright (c) 2013 Zachary Graceffa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZTGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZTGAppDelegate class]));
+    }
+}
